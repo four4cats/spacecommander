015e6fb8b1aaed6c0a11a783dcfbeb0fd983c378
xiaobinlin <xiaobin01.lin@igg.com>
2019-04-10 17:59:33 +0800

    优化配置

--- custom/DictFormatter.py
+++ custom/DictFormatter.py
@@ -7,15 +7,15 @@ def format_dict_trail(m):
     # Unformatted comment statements
     if text.find('//') >= 0:
         return text
-    return re.sub(r',\n\s*}', "}", text)
+    return re.sub(r',\s*}', "}", text)
 
 def pre_format_dict(full_text):
     # optimized nesting dict
-    rst = re.sub(r': +@{\n+ +', ": @{", full_text)
+    rst = re.sub(r': +@{\n+\s+', ": @{", full_text)
     
     # remove '\n' at leading of @{}
     # rst = re.sub(r'@{\n+ +', "@{", rst)
 
     # remove trail ',' at end of @{}
-    rst = re.sub(r'.*: [a-zA-Z0-9_@,}\s\.\(\)\[\]]+,\s*}]?[;\n]', format_dict_trail, rst)
+    rst = re.sub(r'.*: *[a-zA-Z0-9_@,}\s\.\(\)\[\]]+,\s*}]?[;\n]', format_dict_trail, rst)
     return rst
